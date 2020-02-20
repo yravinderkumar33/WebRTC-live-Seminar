cfdd9039161e124921574401e7406cb25c5573b0
ravinder kumar <yravinderkumar33@gmail.com>
2020-02-20 09:46:31 +0530

    signaling server change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   baseUrl: "https://devcon.sunbirded.org",
-  signalingServer: 'https://localhost:9559/',
+  signalingServer: 'https://socketio-over-nodejs2.herokuapp.com:443/',
   bearer: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIyZWU4YTgxNDNiZWE0NDU4YjQxMjcyNTU5ZDBhNTczMiJ9.7m4mIUaiPwh_o9cvJuyZuGrOdkfh0Nm0E_25Cl21kxE'
 };
 
