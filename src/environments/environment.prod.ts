9122b28d86d9dfb9c93d2ebda2d3a142fe8f6465
ravinder kumar <yravinderkumar33@gmail.com>
2020-02-20 10:44:10 +0530

    changing signaling serrver

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
   baseUrl: "https://devcon.sunbirded.org",
-  signalingServer: 'https://socketio-over-nodejs2.herokuapp.com:443/',
+  signalingServer: 'https://10.0.0.108:9559/',
   bearer: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIyZWU4YTgxNDNiZWE0NDU4YjQxMjcyNTU5ZDBhNTczMiJ9.7m4mIUaiPwh_o9cvJuyZuGrOdkfh0Nm0E_25Cl21kxE'
 };
+
+
+// 'https://socketio-over-nodejs2.herokuapp.com:443/',
