287535a28a0186a7db5b74528f47203ed5eaa42f
ravinder kumar <yravinderkumar33@gmail.com>
2020-02-20 10:58:05 +0530

    signaling changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   baseUrl: "https://devcon.sunbirded.org",
-  signalingServer: 'https://10.0.0.108:9559/',
+  signalingServer: 'https://localhost:9559/',
   bearer: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIyZWU4YTgxNDNiZWE0NDU4YjQxMjcyNTU5ZDBhNTczMiJ9.7m4mIUaiPwh_o9cvJuyZuGrOdkfh0Nm0E_25Cl21kxE'
 };
 
